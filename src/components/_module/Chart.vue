8e5aded69aac594c80fb72e4660de1658644c648
ichvanul <ichvanulyp@gmail.com>
2020-06-09 21:00:21 +0700

    first-commit

---
+++ src/components/_module/Chart.vue
@@ -0,0 +1,72 @@
+<template>
+<div class="container-chart">
+  <div class="rectangle">
+    <div class="notes">
+      <h3>Recent Order</h3>
+    </div>
+    <div class="body-chart">
+      <div class="value">
+        <h6>INVOICES</h6>
+        <p>#1101154288</p>
+        <p>#1101154288</p>
+        <p>#1101154288</p>
+      </div>
+      <div class="value">
+        <h6>CASHIER</h6>
+        <p>Ichvanul</p>
+        <p>Vivi Alida</p>
+        <p>Ryan Hidayat</p>
+      </div>
+      <div class="value">
+        <h6>DATE</h6>
+        <p>21 May 2020</p>
+        <p>20 May 2020</p>
+        <p>20 May 2020</p>
+      </div>
+      <div class="value">
+        <h6>ORDERS</h6>
+        <p>Coffee Latte, Steak</p>
+        <p>V60, Bread</p>
+        <p>Cappucino, Fried Fries</p>
+      </div>
+      <div class="value">
+        <h6>AMOUNT</h6>
+        <p>Rp. 63.000</p>
+        <p>Rp. 48.000</p>
+        <p>Rp. 55.000</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Chart',
+};
+</script>
+
+<style lang="scss" scoped>
+.container-chart{
+  margin: 50px 50px 50px 90px;
+  .rectangle {
+    background: linear-gradient(285.38deg, lightgray 30.05%, rgba(255, 143, 178, 0) 133.19%);
+    border-radius: 10px;
+    height: 243px;
+    width: 1245px;
+    padding-top: 30px;
+    padding-bottom: 30px;
+    .notes{
+      h3{
+        font-size: 20px;
+        text-align: center;
+      }
+    }
+    .body-chart{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+    }
+  }
+}
+</style>
---
+++ src/view/register.vue
@@ -0,0 +1,97 @@
+<template>
+<div class="signup">
+    <form>
+    <h2 style="color: #fff;">Register</h2>
+    <input type="text" name="username" placeholder="Name"><br><br>
+    <input type="text" placeholder="Email"><br><br>
+    <input type="password" name="pass" placeholder="Password"><br><br>
+    <input type="button" value="Sign Up"><br><br><br><br>
+    <div class="account"> Do you have account?<router-link to="/"><a
+    style="text-decoration: none; font-family: airbnb; color: #F24F8A;">&nbsp;
+    Log In</a></router-link></div>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+};
+</script>
+
+<style scoped>
+@font-face {
+  font-family: airbnb;
+  src: url('../assets/font/AirbnbCerealBold.ttf')
+}
+
+body {
+    font-family: airbnb;
+    color: #fff;
+    background-size: cover;
+}
+
+.signup {
+    background: #57CAD5;
+    border-radius: 10px;
+    padding:40px;
+    width:320px;
+    margin:auto;
+    margin-top:80px;
+    height:370px;
+    margin-left:180px;
+    margin:0 auto;
+    margin-top:90px;
+}
+
+form {
+    width: 240px;
+    text-align: center;
+}
+
+input {
+    width: 240px;
+    text-align: center;
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid #fff;
+    font-family: 'Play', sans-serif;
+    font-size: 16px;
+    font-weight: 200px;
+    padding: 10px 0;
+    transition: border 0.5s;
+    outline: none;
+    color: #fff;
+}
+
+input[type=button] {
+    border: none;
+    width: 190px;
+    background: #F24F8A;
+    color: white;
+    font-size: 16px;
+    line-height: 25px;
+    padding: 10px 0;
+    border-radius: 15px;
+    cursor: pointer;
+    font-family: airbnb;
+}
+
+input[type=button]:hover {
+    color: #57CAD5;
+    background-color: white;
+}
+
+h2 {
+    color: #000;
+}
+
+::placeholder {
+    color:aliceblue;
+    opacity: 0.8; /* Firefox */
+}
+
+.account {
+  font-family: airbnb;
+}
+</style>
